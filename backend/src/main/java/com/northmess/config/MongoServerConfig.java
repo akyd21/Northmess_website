6bd5c2128ac8e7975fc9655867deba48a044fd60
Akash Raj <akash@gmail.com>
2026-08-05 01:22:14 +0530

    server file update

--- backend/src/main/java/com/northmess/config/MongoServerConfig.java
+++ backend/src/main/java/com/northmess/config/MongoServerConfig.java
@@ -7,8 +7,10 @@ import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 @Slf4j
 public class MongoServerConfig {
 
